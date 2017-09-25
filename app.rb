fe112c0f784a809f2752482ca9edf5af09ca30b0
Yi Xuan Sim <yxuan1996@gmail.com>
2017-09-25 23:37:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Penguin"
+  end
+
+  get '/hometown' do
+    "My hometown is Antartica"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Lalala"
+  end
 end
